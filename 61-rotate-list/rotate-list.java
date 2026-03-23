0073c896a57cea938e404eac2c0864f962a722d2
vanshikadev <vanshikadev23@gmail.com>
2026-03-23 20:44:15 +0530

    Time: 0 ms (100.00%) | Memory: 44.4 MB (37.85%) - LeetSync

--- 61-rotate-list/rotate-list.java
+++ 61-rotate-list/rotate-list.java
@@ -10,49 +10,45 @@
  */
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
-
-        ListNode dummy = new ListNode(-1); 
-        dummy.next = head;
-        ListNode curr = dummy;
-        ListNode prev = dummy;
-        int n = getSize(head);  // O(N)
-        if(k == n || k == 0 || n ==0)
+        if(k == 0 || head == null || head.next == null ) //edge cases
         {
             return head;
         }
-        else
-        {
-            if(k>n)
-        {
-            k = k%n;
+        int size = getSize(head);  // O(N) 
+
+        if(k > size){
+            k = k%size;
+        }
+        if(k == size ){ return head; } // same size. 
+
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+
+        ListNode forw = dummy;
+        ListNode back = dummy;
+
+
+        for(int i = 0 ; i< k; i++ ){
+            forw = forw.next;       // O(K)
+        }
+        while (forw.next != null ){
+            forw= forw.next;        //O(N-k)
+            back = back.next;
         }
 
-        for (int i = 0; i < k; i++)//O(k)
-        {
-            while(curr.next!=null)   // O(N)
-            {
-                prev = curr;
-                curr=curr.next;
-            }
-
-            curr.next = head;
-            dummy.next = curr;
-            head = curr;
-            curr = dummy;
-            prev.next= null;
-
-        }
-
+        forw.next = dummy.next;
+        dummy.next = back.next;
+        back.next = null;
         return dummy.next;
-        }
-        
+
+}
+
+private int getSize(ListNode head){
+    int size = 0; 
+    while(head != null ){
+        size = size +1; 
+        head = head.next;
     }
-    private int getSize(ListNode head) {
-        int size = 0;
-        while (head != null) {
-            size++;
-            head = head.next;
-        }
-        return size;
-    }
+    return size; 
+}
 }
