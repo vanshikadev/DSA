d1e2eca3af3140012089e6cab1c477c90f78501e
vanshikadev <vanshikadev23@gmail.com>
2026-04-05 14:00:02 +0530

    Time: 9 ms (99.15%) | Memory: 59.1 MB (88.34%) - LeetSync

--- 148-sort-list/sort-list.java
+++ 148-sort-list/sort-list.java
@@ -1,44 +1,44 @@
 class Solution {
     public ListNode sortList(ListNode head) {
-        // BASE CASE → constant work
-        if (head == null || head.next == null) return head;
-
-        // 🔽 DIVIDE STEP (find middle)
-        ListNode slow = head, fast = head, prev = null;
-        while (fast != null && fast.next != null) { // O(n)
-            prev = slow;
-            slow = slow.next;
-            fast = fast.next.next;
+        if(head == null || head.next == null){
+            return head;
         }
 
-        prev.next = null; // split list into 2 halves
+        //dividing
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode prev = null;
+        while(fast != null && fast.next != null){
+            prev=slow;
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        prev.next=null;
 
-        // 🔁 RECURSION (divide further)
+        //further divide
         ListNode left = sortList(head);
         ListNode right = sortList(slow);
 
-        // 🔼 MERGE STEP
-        return merge(left, right); // O(n)
+        //merging
+        return merge(left,right);
     }
-
-    private ListNode merge(ListNode l1, ListNode l2) {
+    private ListNode merge(ListNode left,ListNode right){
         ListNode dummy = new ListNode(-1);
         ListNode curr = dummy;
 
-        while (l1 != null && l2 != null) { // O(n)
-            if (l1.val < l2.val) {
-                curr.next = l1;
-                l1 = l1.next;
-            } else {
-                curr.next = l2;
-                l2 = l2.next;
+        while(left != null && right != null){
+            if(left.val >= right.val){
+                curr.next = right;
+                curr = curr.next;
+                right=right.next;
             }
-            curr = curr.next;
+            else{
+                curr.next = left;
+                curr = curr.next;
+                left=left.next;
+            }
         }
-
-        if (l1 != null) curr.next = l1;
-        if (l2 != null) curr.next = l2;
-
+        curr.next = left == null ? right:left;
         return dummy.next;
     }
 }
