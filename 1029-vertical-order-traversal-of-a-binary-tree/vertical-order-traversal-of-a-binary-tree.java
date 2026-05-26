c3b7dd009f10663dd75149a45448f289f5714575
vanshikadev <vanshikadev23@gmail.com>
2026-05-26 07:14:19 +0530

    Time: 5 ms (12.22%) | Memory: 44.4 MB (21.50%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.java
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.java
@@ -14,50 +14,47 @@
  * }
  */
 class Solution {
-    class Pair {
+    class Pair{
         TreeNode node;
         int row;
-        int column;
-
-        Pair(TreeNode node, int row, int column) {
+        int col;
+        Pair(TreeNode node , int row, int col){
             this.node = node;
             this.row = row;
-            this.column = column;
+            this.col = col;
         }
     }
-
     public List<List<Integer>> verticalTraversal(TreeNode root) {
-        TreeMap<Integer , TreeMap<Integer , PriorityQueue<Integer>>> map = new TreeMap<>();
-        Queue<Pair> queue = new LinkedList<>();
         List<List<Integer>> result = new ArrayList<>();
         if(root == null){
             return result;
         }
-        queue.offer(new Pair(root,0,0));
-        while(!queue.isEmpty()){
-            Pair pair = queue.poll();
-            int column = pair.column;
+        TreeMap<Integer,TreeMap<Integer,PriorityQueue<Integer>>> rowcolmap = new TreeMap<>();
+        Stack<Pair> stack = new Stack<>();
+        stack.push(new Pair(root,0,0));
+        while(!stack.isEmpty()){
+            Pair pair = stack.pop();
             int row = pair.row;
+            int col = pair.col;
             TreeNode node = pair.node;
-
+            rowcolmap.putIfAbsent(col,new TreeMap<Integer,PriorityQueue<Integer>>());
+            rowcolmap.get(col).putIfAbsent(row, new PriorityQueue<Integer>());
+            rowcolmap.get(col).get(row).offer(node.val);
             if(node.left != null){
-                queue.offer(new Pair(node.left , row + 1 , column -1));
+                stack.push(new Pair(node.left, row+1, col-1));
             }
             if(node.right != null){
-                queue.offer(new Pair(node.right , row + 1 , column + 1));
+                stack.push(new Pair(node.right, row+1, col+1));
             }
-            map.putIfAbsent(column, new TreeMap<Integer,PriorityQueue<Integer>>());
-            map.get(column).putIfAbsent(row, new PriorityQueue<Integer>());
-            map.get(column).get(row).offer(node.val);
         }
-        for(TreeMap<Integer, PriorityQueue<Integer>> rows : map.values()){
-            List<Integer> list = new ArrayList<>();
-            for(PriorityQueue<Integer> que : rows.values()){
-                while(!que.isEmpty()){
-                    list.add(que.poll());
+        for(TreeMap<Integer, PriorityQueue<Integer>> row : rowcolmap.values()){
+            List<Integer> res = new ArrayList<>();
+            for( PriorityQueue<Integer> pq : row.values()){
+                while(!pq.isEmpty()){
+                    res.add(pq.poll());
                 }
             }
-        result.add(list);
+            result.add(new ArrayList<>(res));
         }
         return result;
     }
