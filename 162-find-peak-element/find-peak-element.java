701c7bfb2df7820a5bc7db1b4755eb9fbc3b937a
vanshikadev <vanshikadev23@gmail.com>
2026-06-17 11:01:21 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (54.86%) - LeetSync

--- 162-find-peak-element/find-peak-element.java
+++ 162-find-peak-element/find-peak-element.java
@@ -4,7 +4,7 @@ class Solution {
         int high = nums.length-1;
         while(low < high){
             int mid = low + (high - low) / 2; 
-            if(nums[mid] < nums[mid+1]){
+            if(mid < nums.length-1 && nums[mid] < nums[mid+1]){
                 low = mid + 1;
             } 
             else{
