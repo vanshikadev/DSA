f1307c4ee22fe7c5bccce3da7cbf44d17ff6677a
vanshikadev <vanshikadev23@gmail.com>
2026-08-05 21:24:07 +0530

    Time: 5 ms (27.94%) | Memory: 51.5 MB (83.59%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,56 +1,47 @@
 class Solution {
-
+    int [][] directions = {
+        {1,0},
+        {-1,0},
+        {0,1},
+        {0,-1}
+    };
     public int numIslands(char[][] grid) {
-
         int row = grid.length;
         int column = grid[0].length;
+        int result = 0;
 
-        int island = 0;
-
-        for (int i = 0; i < row; i++) {
-
-            for (int j = 0; j < column; j++) {
-
-                if (grid[i][j] == '1') {
-
-                    island++;
+        for(int i = 0; i< row; i++){
+            for(int j = 0; j< column; j++){
+                if(grid[i][j] == '1'){
+                    result += 1;
                     bfs(grid, i, j);
                 }
             }
         }
-
-        return island;
+        return result;
     }
-
-    private void bfs(char[][] grid, int r, int c) {
+    private void bfs(char[][] grid , int i, int j){
         Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{i,j});
+        grid[i][j] = '0';
 
-        queue.offer(new int[]{r,c});
-        grid[r][c] = '0';
-
-        int [][] directions = {
-            {1,0},
-            {-1,0},
-            {0,-1},
-            {0,1}
-        };
         while(!queue.isEmpty()){
-            int [] cordinates= queue.poll();
-            int row = cordinates[0];
-            int col = cordinates[1];
+            int[] node = queue.poll();
+            int r = node[0];
+            int c = node[1];
+            for(int[] dir : directions) {
+            int newrow = r + dir[0];
+            int newcolumn = c + dir[1];
 
-            if(row < 0 || row >= grid.length || col < 0 || col >= grid[row].length){
+            if(newrow < 0 || newrow >= grid.length || newcolumn < 0 || newcolumn >=grid[0].length){
                 continue;
             }
-            for(int[] dir : directions){
-                if(row + dir[0] < 0 || row + dir[0] >= grid.length || col + dir[1] < 0 || col + dir[1] >= grid[row].length){
+            if(grid[newrow][newcolumn] == '0'){
                 continue;
             }
-                if(!(grid[row + dir[0]][col + dir[1]] == '0')){
-                    queue.offer(new int[]{row + dir[0],col + dir[1]});
-                    grid[row + dir[0]][col + dir[1]] = '0';
-                }
-            }
+            grid[newrow][newcolumn] = '0';
+            queue.offer(new int[]{newrow,newcolumn});
+        }
         }
     }
 }
