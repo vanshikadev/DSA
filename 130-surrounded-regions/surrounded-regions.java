e9224ffd9ba75c8211bcc3779f2ef30b40083537
vanshikadev <vanshikadev23@gmail.com>
2026-08-07 21:18:40 +0530

    Time: 4 ms (15.07%) | Memory: 47.7 MB (76.13%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.java
+++ 130-surrounded-regions/surrounded-regions.java
@@ -8,28 +8,35 @@ class Solution {
     public void solve(char[][] board) {
         int row = board.length;
         int column = board[0].length;
+        Queue<int[]> queue = new LinkedList<>();
         if(row < 3 || column < 3){
             return;
         }
         for(int i = 0; i<row; i++){
             if(board[i][0]=='O'){
-                dfs(board,i,0,row,column);
+                board[i][0] = 'V';
+                queue.offer(new int[]{i,0});
+                
             }
             if(board[i][column-1] == 'O'){
-                dfs(board,i,column-1,row,column);
+                board[i][column-1] = 'V';
+                queue.offer(new int[]{i,column-1});
             }
         }
         for(int j = 0; j<column; j++){
             if(board[0][j]=='O'){
-                dfs(board,0,j,row,column);
+                board[0][j] = 'V';
+                queue.offer(new int[]{0,j});
             }
             if(board[row-1][j] == 'O'){
-                dfs(board,row-1,j,row,column);
+                board[row-1][j] = 'V';
+                queue.offer(new int[]{row-1,j});
             }
         }
+        bfs(board,queue,row,column);
         for(int k=0;k<row;k++){
             for(int l=0;l<column;l++){
-                if(board[k][l]=='S'){
+                if(board[k][l]=='V'){
                     board[k][l] = 'O';
                 }
                 else if(board[k][l]=='O'){
@@ -40,17 +47,22 @@ class Solution {
         }
         return;
     }
-    private void dfs(char[][] board, int i, int j, int row, int column){
-        board[i][j] = 'S';
-        for(int[] dir: directions){
-            int newrow = i + dir[0];
-            int newcolumn = j + dir[1];
+    private void bfs(char[][] board, Queue<int[]> queue, int row, int column){
+        while(!queue.isEmpty()){
+            int[] tuple = queue.poll();
+            for(int[] dir: directions){
+
+            int newrow = tuple[0] + dir[0];
+            int newcolumn = tuple[1] + dir[1];
+
             if(newrow < 0 || newrow >= row|| newcolumn < 0 || newcolumn >= column){
                 continue;
             }
             if(board[newrow][newcolumn] == 'O'){
-                dfs(board, newrow, newcolumn, row,column);
+                board[newrow][newcolumn] = 'V';
+                queue.offer(new int[]{newrow,newcolumn});
             }
         } 
+        }
     }
 }
