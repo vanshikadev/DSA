392ab6593b8fe73734278045ba8302103cb70484
vanshikadev <vanshikadev23@gmail.com>
2026-08-07 20:31:40 +0530

    Time: 2 ms (11.25%) | Memory: 46.8 MB (44.03%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,8 +1,8 @@
 class Solution {
     int[][] directions = {
-        {1,0},
         {0,1},
         {0,-1},
+        {1,0},
         {-1,0}
     };
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
@@ -13,21 +13,28 @@ class Solution {
             return image;
         }
         int oldcolor = image[sr][sc];
-        dfs(image, sr,sc, color,oldcolor);
+        bfs(image, sr,sc, color,oldcolor);
         return image;
     }
-    private void dfs(int[][] image, int sr, int sc, int color, int oldcolor){
+    private void bfs(int[][] image, int sr, int sc, int color, int oldcolor){
+        Queue<int[]> queue = new LinkedList<>();
+        
+        queue.offer(new int[]{sr,sc});
         image[sr][sc] = color;
-        for(int[] dir : directions){
-            int r = sr + dir[0];
-            int c = sc + dir[1];
+
+        while(!queue.isEmpty()){
+            int[] tuple = queue.poll();
+            for(int[] dir : directions){
+            int r = tuple[0] + dir[0];
+            int c = tuple[1] + dir[1];
             if(r < 0 || r >= image.length || c < 0 || c >= image[0].length){
                 continue;
             }
             if(image[r][c] == oldcolor){
                 image[r][c] = color;
-                dfs(image, r,c, color,oldcolor);
+                queue.offer(new int[]{r,c});
             }
+        }
         }
     }
 }
