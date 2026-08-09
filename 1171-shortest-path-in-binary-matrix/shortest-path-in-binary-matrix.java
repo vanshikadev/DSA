8fd9b02ad383ee46d91cd819ea2d94197eb26d54
vanshikadev <vanshikadev23@gmail.com>
2026-08-09 23:20:03 +0530

    Time: 18 ms (31.15%) | Memory: 47.8 MB (46.79%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
@@ -1,46 +1,51 @@
 class Solution {
-    int [][] directions={
-        {1,0},
+    int [][] directions = {
         {0,1},
-        {0,-1},
+        {1,0},
         {-1,0},
+        {0,-1},
         {-1,-1},
-        {-1,1},
+        {1,1},
         {1,-1},
-        {1,1}
+        {-1,1}
     };
     public int shortestPathBinaryMatrix(int[][] grid) {
-        int count=1;
-        Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[]{0,0});
-        int n = grid.length;
-        if(grid[0][0] == 1 || grid[n-1][n-1] == 1){
+        int row = grid.length;
+        int column = grid[0].length;
+        int circle = 1;
+        if(grid[0][0] == 1 || grid[row-1][column-1] == 1){
             return -1;
         }
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{0,0});
         grid[0][0] = 1;
+        if(row == 1){
+            return 1;
+        }
+
         while(!queue.isEmpty()){
             int size = queue.size();
-            for(int i =0; i < size; i++){
-                int[] tuple= queue.poll();
-                if(tuple[0] == grid.length-1 && tuple[1] == grid[0].length-1){
-                    return count;
-                }
+            for(int s =0; s< size; s++){
+                int[] tuple = queue.poll();
                 for(int[] dir: directions){
-                    int newrow = tuple[0] + dir[0];
-                    int newcol = tuple[1] + dir[1];
-
-                    if(newrow < 0 || newrow >= grid.length || newcol < 0 || newcol >= grid[0].length){
+                    int r = tuple[0] + dir[0];
+                    int c = tuple[1] + dir[1];
+                    if(r < 0 || r >=row || c<0 ||c>=column){
                         continue;
                     }
-                    
-                    if(grid[newrow][newcol] == 1){
+                    if(grid[r][c] == 1){
                         continue;
                     }
-                    grid[newrow][newcol] = 1;
-                    queue.offer(new int[]{newrow,newcol});
+                    if(grid[r][c] == 0 && r == row-1 && c == column-1){
+                        return ++circle;
+                    }
+                    if(grid[r][c] == 0){
+                        grid[r][c] = 1;
+                        queue.offer(new int[]{r,c});
+                    }
                 }
             }
-            count += 1;
+            circle++;
         }
         return -1;
     }
