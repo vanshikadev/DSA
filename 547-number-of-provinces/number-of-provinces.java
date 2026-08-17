2bcf426d49187e46d4c24d4c5ffc410a4366a923
vanshikadev <vanshikadev23@gmail.com>
2026-08-18 00:04:10 +0530

    Time: 0 ms (100.00%) | Memory: 46.7 MB (98.47%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,32 +1,32 @@
 class Solution {
     public int findCircleNum(int[][] isConnected) {
-        int n = isConnected.length;
+        int n = isConnected.length; //no. of nodes
         boolean[] visited = new boolean[n];
-        int result = 0;
-        for(int i =0; i< n; i++){
+        int answer =0;
+        for(int i = 0 ; i<n ;i++)
+        {
             if(!visited[i]){
-                result += 1;
-                bfs(isConnected, visited, i);
+                answer++;
+                dfs(isConnected , visited , n,i);
             }
         }
-        return result;
+        return answer;
     }
-    private void bfs(int[][] isConnected , boolean[] visited, int i){
-        Queue<Integer> queue = new LinkedList<>();
-        queue.offer(i);
+    private void dfs(int[][] isConnected, boolean[] visited, int n, int i){
+        // Queue<Integer> queue = new LinkedList<>();
+        // queue.offer(i);
         visited[i] = true;
-
-        while(!queue.isEmpty()){
-            int n = queue.size();
-            for(int k = 0; k< n; k++){
-                int node = queue.poll();
-                for(int j = 0; j<isConnected.length; j++){
-                    if(!visited[j] && isConnected[node][j] == 1){
-                        queue.offer(j);
-                        visited[j] = true;
+        // int size = queue.size();
+        // for(int j = 0; j< size; j++){
+            // while(!queue.isEmpty()){
+                // int node = queue.poll();
+                for(int k =0; k<n; k++){
+                    if(!visited[k] && isConnected[i][k] == 1){
+                        visited[k] = true;
+                        dfs(isConnected , visited , n,k);
                     }
-                }
-            }
+                // }
+            // }
         }
     }
 }
