066d0bd83409a4eafedda04eb2e9fc081e333bb0
vanshikadev <vanshikadev23@gmail.com>
2026-08-18 01:12:42 +0530

    Time: 14 ms (71.50%) | Memory: 47.3 MB (91.56%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
@@ -1,51 +1,49 @@
 class Solution {
-    int [][] directions = {
-        {0,1},
+    int[][] directions = {
         {1,0},
+        {0,1},
         {-1,0},
         {0,-1},
-        {-1,-1},
         {1,1},
+        {-1,-1},
         {1,-1},
         {-1,1}
     };
     public int shortestPathBinaryMatrix(int[][] grid) {
         int row = grid.length;
         int column = grid[0].length;
-        int circle = 1;
-        if(grid[0][0] == 1 || grid[row-1][column-1] == 1){
+        int level = 1;
+        if(grid[0][0] != 0 || grid[row-1][column-1] != 0){
             return -1;
         }
+        if(row == 1 && column == 1){
+    return 1;
+}
         Queue<int[]> queue = new LinkedList<>();
         queue.offer(new int[]{0,0});
         grid[0][0] = 1;
-        if(row == 1){
-            return 1;
-        }
-
-        while(!queue.isEmpty()){
-            int size = queue.size();
-            for(int s =0; s< size; s++){
+            while(!queue.isEmpty()){
+                int size = queue.size();
+                for(int i = 0; i<size; i++){
                 int[] tuple = queue.poll();
-                for(int[] dir: directions){
+                for(int[] dir : directions){
                     int r = tuple[0] + dir[0];
                     int c = tuple[1] + dir[1];
-                    if(r < 0 || r >=row || c<0 ||c>=column){
+
+                    if(r <0 || r>= row || c< 0 || c>= column){
                         continue;
                     }
-                    if(grid[r][c] == 1){
-                        continue;
-                    }
-                    if(grid[r][c] == 0 && r == row-1 && c == column-1){
-                        return ++circle;
+                    if(grid[r][c] == 0 && r == row-1 && c == column -1){
+                        level = level+1;
+                        return level;
                     }
                     if(grid[r][c] == 0){
-                        grid[r][c] = 1;
                         queue.offer(new int[]{r,c});
+                        grid[r][c] = 1;
                     }
                 }
-            }
-            circle++;
+                }
+                level++;
         }
         return -1;
     }
