20843b7771801bb26e36af9a502fd6e1a4ac4134
vanshikadev <vanshikadev23@gmail.com>
2026-09-10 20:56:37 +0530

    Time: 2 ms (88.41%) | Memory: 44.3 MB (39.65%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,56 +1,57 @@
 class Solution {
     int [][] directions = {
-        {0,1},
         {1,0},
+        {0,1},
         {-1,0},
         {0,-1}
     };
     public int orangesRotting(int[][] grid) {
-        int row = grid.length;
-        int column = grid[0].length;
+        int m = grid.length;
+        int n = grid[0].length;
+        int fresh =0;
         int rotten = 0;
-        int fresh = 0;
-        int ans = 0;
+        int time = 0;
         Queue<int[]> queue = new LinkedList<>();
-        for(int i = 0; i < row; i++){
-            for(int j =0; j<column; j++){
-                if(grid[i][j] == 2){
-                    queue.offer(new int[]{i,j});
-                    rotten++;
-                }
-                else if(grid[i][j] == 1){
+        for(int i =0;i<m;i++){
+            for(int j =0;j<n;j++){
+                if(grid[i][j] == 1){
                     fresh++;
+                }
+                else if(grid[i][j] == 2){
+                    rotten++;
+                    queue.offer(new int[]{i,j});
                 }
             }
         }
-        if(fresh == 0){
-            return ans;
+         if(fresh == 0){
+            return 0;
         }
         if(rotten == 0){
             return -1;
         }
         while(!queue.isEmpty()){
             int size = queue.size();
-            for(int i =0; i< size; i++){
+            for(int i =0; i<size; i++){
                 int[] tuple = queue.poll();
                 for(int[] dir : directions){
                     int r = tuple[0] + dir[0];
                     int c = tuple[1] + dir[1];
-                    if(r < 0 || r >= row || c < 0 || c >= column){
+                    if(r < 0 || r>= m || c<0 || c>= n){
                         continue;
                     }
                     if(grid[r][c] == 1){
-                        grid[r][c] = 2;
                         fresh--;
+                        grid[r][c] = 2;
                         queue.offer(new int[]{r,c});
+                    }
+                    if(fresh == 0){
+                        return time+1;
                     }
                 }
             }
-            ans++;
-            if(fresh == 0){
-                return ans;
-            }
+            time++;
         }
         return -1;
     }
+
 }
